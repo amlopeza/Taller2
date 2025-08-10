9970e106ab3381393fbfaae66bad39cade94d2ee
Ana Maria <amlopeza14@gmail.com>
2025-08-10 18:06:43 -0400

    update load_data function and time using to)_datetime() from file app.py

--- app.py
+++ app.py
@@ -21,7 +21,12 @@ app.config.suppress_callback_exceptions = True
 
 # Load data from csv
 def load_data():
-    # To do: Completar la función 
+    # Cargar datos desde el archivo CSV
+    df = pd.read_csv('datos_energia.csv')
+    # Convertir la columna de tiempo a datetime y establecer como índice
+    df['time'] = pd.to_datetime(df['time'])
+    df= df.set_index('time')
+    return df
     
 
 # Cargar datos
@@ -240,4 +245,4 @@ def update_output_div(date, hour, proy):
 
 # Run the server
 if __name__ == "__main__":
-    app.run_server(debug=True)
+    app.run(debug=True)
